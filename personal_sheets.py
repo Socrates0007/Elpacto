17b6e2e255a88848d5cc149a86850fc5f91d924a
Socrates0007 Udosen <akanavaro@gmail.com>
2025-08-21 19:51:31 +0000

    added cs information

---
+++ personal_sheets.py
@@ -0,0 +1,166 @@
+# personal_sheets.py
+# Each personal sheet MUST share the same header as in config.HEADERS.
+
+PEOPLE = [
+    {
+            "name": "Joy",
+            "sheet_id": "1XQM3OSDmZqrfzFFmDN-g-OYTS5RQf4T1JBJGa_-mw9w",
+            "whatsapp": "+2349036090803",
+                                },
+                          
+    {
+        "name": "Grace",
+        "sheet_id": "1-DoI6ZvUb_RmweKfiJ-pfy87wiqPn9T_rd8iLXx9RMg",
+        "whatsapp": "+2348071669947",
+              },
+    {
+        "name": "Faith",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              "sheet_id": "1TwaUgU7XN9pUOw8j6URw_mk08e7JGtdcqm4-EV0jpoY",
+                                                                                            "whatsapp": "+2349076863604",
+                                                                                                },
+                                                                                                ]
